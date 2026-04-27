27faecb1d9dc51da13cef5f216c2ff0022792191
muslim-maker <muslimvasutkin@gmail.com>
2026-04-27 11:20:39 +0500

    Update customer_metrics.sql

--- sql/customer_metrics.sql
+++ sql/customer_metrics.sql
@@ -1,1 +1,21 @@
+WITH order_payments AS (
+    SELECT 
+        order_id, 
+        SUM(payment_value) AS order_revenue
+    FROM olist_order_payments_dataset
+    GROUP BY order_id
+)
 
+SELECT 
+    c.customer_unique_id, 
+    COUNT(DISTINCT o.order_id) AS total_orders, 
+    SUM(op.order_revenue) AS total_revenue,
+    AVG(op.order_revenue) AS avg_check
+FROM order_payments op
+JOIN olist_orders_dataset o USING(order_id)
+JOIN olist_customers_dataset c USING(customer_id)
+
+WHERE o.order_status = 'delivered'
+
+GROUP BY c.customer_unique_id
+ORDER BY total_revenue DESC, total_orders DESC;
